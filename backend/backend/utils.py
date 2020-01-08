71ae69edb0120d9b66615d0a1c7b1e6ab6691a7e
Oribe Abad <oribe@insaite.io>
2020-01-08 17:23:05 -0600

    make rethinkdb connection for save user

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -3,12 +3,13 @@ from rethinkdb import r
 import os
 
 user_schema = Schema({'user':str,'password':str})
+users_table = os.getenv("USERS_TABLE","users")
 
 def get_rethink():
     return r.connect(
         host=os.getenv("RETHINK_HOST","127.0.0.1"),
-        port=os.getenv("RETHINK_PORT",28015)
-        db='chat'
+        port=os.getenv("RETHINK_PORT",28015),
+        db=os.getenv("RETHINK_DB",'test')
     )
 
 def validate_user_post(post_user):
@@ -21,6 +22,6 @@ def validate_user_post(post_user):
     return True
 
 def save_user(user):
-    session = get_rethink()
-    table = session.table('users')
-    table.insert(user).run(session)
+    con = get_rethink()
+    r.table(users_table).insert(user).run(con)
+    con.close()
