f0fa9c108766f6da189f08de8878f987b70fd708
Oribe Abad <oribe@insaite.io>
2020-01-08 16:59:42 -0600

    add pylint to dev dependencies

--- backend/backend/services.py
+++ backend/backend/services.py
@@ -9,6 +9,5 @@ def home():
         return "invalid json request",400
     if not validate_user_post(request.json):
         return "invalid json body",400
-
-
+    save_user(request.json)
     return "hola"
