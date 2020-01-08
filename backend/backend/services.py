7df33cb92d9ce0c41da4c3bf7fa75ae6c15f5b61
Oribe Abad <oribe@insaite.io>
2020-01-08 16:56:07 -0600

    add request validator

--- backend/backend/services.py
+++ backend/backend/services.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask,request,Response
+from .utils import validate_user_post,save_user
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/user",methods=['POST'])
 def home():
-    return "Hello world"
+    if not request.data:
+        return "invalid json request",400
+    if not validate_user_post(request.json):
+        return "invalid json body",400
+
+
+    return "hola"
